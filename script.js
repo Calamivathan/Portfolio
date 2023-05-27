131f6d3baba7b8752fe4d89019970cbb5fe75a56
CICADA1857 <97376730+Calamivathan@users.noreply.github.com>
2023-05-27 12:41:42 +0530

    a

---
+++ script.js
@@ -0,0 +1,48 @@
+// var element = document.querySelector('.bm-t-i');
+// var element2 = document.querySelector('.bm-t-i2');
+// var percentage = 0;
+// var increment = 0.0001;
+
+// function updateTransform() {
+//     element.style.transform = `translate(-${percentage}%, 0%) translate3d(0px, 0px, 0px)`;
+//     element2.style.transform = `translate(-${percentage}%, 0%) translate3d(0px, 0px, 0px)`;
+// }
+
+// function incrementPercentage() {
+//     percentage += increment;
+//     if (percentage > 100) {
+//         percentage = 0;
+//     }
+//     updateTransform();
+    
+//     // Call the function again to keep it running
+//     incrementPercentage();
+// }
+
+// // Start the function
+// incrementPercentage();
+    // while (percentage=101) {
+    //     incrementPercentage();
+    // }
+    // setInterval(incrementPercentage, 10); // Change increment speed here (in milliseconds)
+    // incrementPercentage();
+    // var element = document.querySelector('.bm-t-i');
+    // var percentage = 0;
+    // var increment = 0.1;
+    // var element2 = document.querySelector('.bm-t-i2');
+
+    // function updateTransform() {
+    //     element.style.transform = `translate(-${percentage}%, 0%) translate3d(0px, 0px, 0px)`;
+    //     element2.style.transform = `translate(-${percentage}%, 0%) translate3d(0px, 0px, 0px)`;
+    // }
+
+    // function incrementPercentage() {
+    //     percentage += increment;
+    //     if (percentage > 100) {
+    //         percentage = 0;
+    //     }
+    //     updateTransform();
+    //     requestAnimationFrame(incrementPercentage);
+    // }
+
+    // incrementPercentage();
